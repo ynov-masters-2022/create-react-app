9d1380342e47f53cbf0ee2de88fd3dd0ac9e7a85
Lucas Delobelle <lucas@ld-web.net>
2022-10-13 13:01:24 +0200

    :sparkles: add references to newUser inputs

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import USERS from "../data/users";
 import User from "./User";
 
 const Users = () => {
   const [usersList, setUsersList] = useState(USERS);
-  const [newUser, setNewUser] = useState({
-    name: "",
-    firstname: "",
-  });
-
-  const onChange = (e) => {
-    const newUserNext = {
-      ...newUser,
-      [e.target.name]: e.target.value,
-    };
-    setNewUser(newUserNext);
-  };
+  const nameRef = useRef(null);
+  const firstnameRef = useRef(null);
+  console.log("rendering");
 
   const onAddUser = () => {
     const id = Math.max(...usersList.map((u) => u.id)) + 1;
@@ -24,8 +15,8 @@ const Users = () => {
       ...usersList,
       {
         id,
-        name: newUser.name,
-        firstname: newUser.firstname,
+        name: nameRef.current.value,
+        firstname: firstnameRef.current.value,
       },
     ];
     setUsersList(newUsers);
@@ -33,13 +24,8 @@ const Users = () => {
 
   return (
     <>
-      <input type="text" name="name" value={newUser.name} onChange={onChange} />
-      <input
-        type="text"
-        name="firstname"
-        value={newUser.firstname}
-        onChange={onChange}
-      />
+      <input type="text" ref={nameRef} />
+      <input type="text" ref={firstnameRef} />
       <button onClick={onAddUser}>Add User</button>
       <div>
         {usersList.map((user) => (
