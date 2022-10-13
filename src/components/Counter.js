a90a691d665cef242d9eb16bda64acea69bff9a7
Lucas Delobelle <lucas@ld-web.net>
2022-10-13 09:53:22 +0200

    :sparkles: setCounter w/ callback

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,11 +6,16 @@ const Counter = () => {
 
   const decrement = () => setCounter(counter - 1);
   const increment = () => setCounter(counter + 1);
+  const addTwo = () => {
+    setCounter(counter + 1);
+    setCounter((c) => c + 1);
+  };
 
   return (
     <p>
       Compteur : {counter}
       <CounterActions decrement={decrement} increment={increment} />
+      <button onClick={addTwo}>Add two</button>
     </p>
   );
 };
